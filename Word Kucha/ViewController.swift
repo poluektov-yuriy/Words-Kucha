dd7595470ac723ab83c011ec7b19a6bbd0f14ae0
Poluektov Yuriy <poluektov.yuriy@gmail.com>
2022-01-17 16:12:42 +0300

    Initial Commit

---
+++ Word Kucha/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Word Kucha
+//
+//  Created by Yurii Poluektov on 17.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
